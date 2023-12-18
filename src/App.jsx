3fcd380657de83d6bb2d8ed3577c714ad8d48357
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-18 23:18:31 +0530

    color style added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import ToDoItem from "./components/ToDoItem"
 function App() {
   return (
     <>
-     <ToDoItem taskName='01' stat='Done'/>
-     <ToDoItem taskName="02" stat="ToDo"/>
+     <ToDoItem taskName='01' stat='Done' btnColor="green"/>
+     <ToDoItem taskName="02" stat="ToDo" btnColor="blue"/>
     </>
   )
 }
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,9 +1,9 @@
-function ToDoItem({taskName,stat}) {  //argument pass wenne object wise
+function ToDoItem({taskName,stat,btnColor}) {  //argument pass wenne object wise
     
     return ( 
         <div>
             <h3><span>Task </span>{taskName}</h3>
-            <button>{stat}</button>
+            <button style={{backgroundColor:btnColor}}>{stat}</button>
             
         </div>
      );
